4a445189c7cf7a3cb0c36edd7d7dc96fec44ea7e
Francis Otani <oottaanniies@gmail.com>
2021-10-23 13:36:52 +0900

    Feature of estimating total time added

--- main.cpp
+++ main.cpp
@@ -78,15 +78,21 @@ int main( int argc, char** argv )
     }*/
     //std::filesystem::remove_all("data");
    // std::filesystem::create_directory("data");
+    
+    // 1: self-consistent, 2: radiation dominant, 3: matter dominant -> No Output of vti files for the field
+    //  0: no expansion -> Output vti files for the field
     if(expansion){
     }else{
        write_VTK_f( f[0], "field", -1 );
     }
     
-        
+    // Calculate all necessary data: energy density, field average, etc.
 	Energy energy( &field, &leapfrog, f, df );
-   write_VTK_ed( energy.value[0], "energy", -1  );
     
+    // Output vti files for the energy density of the field
+    write_VTK_ed( energy.value[0], "energy", -1  );
+    
+    // Write data to status.txt
 	write_status( &field, &leapfrog, &energy, f, t0 );
 
 	//--------------------------------------------------
@@ -101,45 +107,76 @@ int main( int argc, char** argv )
 
 	start = std::chrono::high_resolution_clock::now();
     
-    for( int loop = 0; loop < max_loop; ++loop )
+    for( int loop = 0; loop < max_loop; ++loop ) // This many times vti files will be created
 	{
         //std::cout << leapfrog.da() << std::endl;
 	    double t = t0 + loop*output_step*dt;
+        
 	    loop_start = std::chrono::high_resolution_clock::now();
 
-            for( int st_loop = 0; st_loop < st_max_loop; ++st_loop ){
+        for( int st_loop = 0; st_loop < st_max_loop; ++st_loop ) // This many times data will be added to status.txt between the output of vti files
+        {
             
-        if(expansion){
+                
+        // 1: self-consistent, 2: radiation dominant, 3: matter dominant -> Compute using the function that takes expansion into account
+        //  0: no expansion -> Compute using the function that doesn't take expansion into account
+            if(expansion){
+                
+            leapfrog.evolution_expansion( &field, f, df, t );
+                
+            }else{
+                leapfrog.evolution( &field, f, df );
+            }
             
-        leapfrog.evolution_expansion( &field, f, df, t );
-            
-        }else{
-            leapfrog.evolution( &field, f, df );
-        }
-		
+            // Calculate all necessary data regarding energy density.
             Energy energy( &field, &leapfrog, f, df );
-        //  std::cout << "t1 = " << t << std::endl;
-           write_status( &field, &leapfrog, &energy, f, t+st_output_step*dt );
-          //  std::cout << "t2 = " << t << std::endl;
-             t = t + st_output_step*dt;
-           // std::cout << "t3 = " << t << std::endl;
+            //  std::cout << "t1 = " << t << std::endl;
+            
+            // Add data to status.txt
+            write_status( &field, &leapfrog, &energy, f, t+st_output_step*dt );
+            //  std::cout << "t2 = " << t << std::endl;
+            
+            // Evolve time
+            t = t + st_output_step*dt;
+            // std::cout << "t3 = " << t << std::endl;
         }
-		  Energy energy( &field, &leapfrog, f, df );
         
+        
+        // Calculate all necessary data regarding energy density.
+        Energy energy( &field, &leapfrog, f, df );
+        
+        
+        // 1: self-consistent, 2: radiation dominant, 3: matter dominant -> No Output of vti files for the field
+        //  0: no expansion -> Output vti files for the field
         if(expansion){
         }else{
             write_VTK_f( f[0], "field", loop );
         }
         
+        
+        // Output vti files for the energy density of the field
 		write_VTK_ed( energy.value[0], "energy", loop );
        
 		//write_status( &field, &leapfrog, &energy, f, t+output_step*dt );
 
+        //Calculate the elapsed time between the output of vti files
         current = std::chrono::high_resolution_clock::now();
 	    elapsed = std::chrono::duration_cast<std::chrono::milliseconds>( current - loop_start );
-        Logout( " Loop %d/%d: %2.3f s \n", loop+1, max_loop, elapsed.count()*1.e-3 );
+        
+        if(loop)
+        {
+            Logout( " Loop %d/%d: %2.3f s \n", loop+1, max_loop, elapsed.count()*1.e-3 );
+        
+        }else{ // This branch is run only for the first loop (loop = 0) to calculate the total estimate time of the simulation judging from the first loop.
+        
+            double hourresidue_estimate = fmod(elapsed.count()*max_loop*1.e-3, 3600);
+            
+            Logout( " Loop %d/%d: %2.3f s \t Estimated total time: %d h %d m %2.3f s \n", loop+1, max_loop, elapsed.count()*1.e-3, int(elapsed.count()*1.e-3)/3600,int(hourresidue_estimate)/60, fmod(hourresidue_estimate, 60.0) );
+        }
     }
-
+    
+    
+    //Calculate total elapsed time
     current = std::chrono::high_resolution_clock::now();
 	elapsed = std::chrono::duration_cast<std::chrono::milliseconds>( current - start );
     double hourresidue = fmod(elapsed.count()*1.e-3, 3600);
